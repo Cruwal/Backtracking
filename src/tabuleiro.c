2bb3b7892cc46de0ad01506e3bf4037d92100824
Cruwal <wallace.souza@usp.br>
2018-09-13 14:03:19 -0300

    Criando tabuleiro

--- src/tabuleiro.c
+++ src/tabuleiro.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <tabuleiro.h>
 
-struct peca{
+struct peca{ //Um vetor de peças é considerado o tabuleiro
   char tipo;  //tipo da peca (PEDRA PAPEL E TESOURA)
   int x, y; //posicao da peca
 };
+
+peca_t *create_tabuleiro(int nLinhas, int nColunas){
+  peca_t *tabuleiro = (peca_t *) malloc(sizeof(peca_t) * (nlinhas * nColunas));
+
+  return tabuleiro;
+}
+
+void insert_peca(peca_t *tabuleiro, int posX, int posY){
+  
+}
