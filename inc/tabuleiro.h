7c3a3da56c0febd1e9bead80e7986fd032ad5bc8
Cruwal <wallace.souza@usp.br>
2018-09-13 14:23:34 -0300

    Tabuleiro

--- inc/tabuleiro.h
+++ inc/tabuleiro.h
@@ -1,6 +1,11 @@
 #ifndef _TABULEIRO_H_
 #define _TABULEIRO_H_
 
-typedef struct peca peca_t;
+typedef struct tabuleiro tab_t;
+
+tab_t *create_tabuleiro(int nLinhas, int nColunas);
+void insert_peca(tab_t *tabuleiro, int posX, int posY, char peca);
+void print_tabuleiro(tab_t *tabuleiro);
+void *delete_tabuleiro(tab_t **tabuleiro);
 
 #endif
